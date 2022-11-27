1153699431a44cc989957c2dce64dcdc56dde929
FabioMen10 <fabiomendesilva@gmail.com>
2022-11-27 12:38:06 -0400

    Started a new book

---
+++ L2PwC/P11Square.c
@@ -0,0 +1,8 @@
+#include <studio.h>
+int main() {
+   int a, s;
+   printf("Enter length of side: ");
+   scanf("%d", &s); //store lengh in s
+   a = s * s; //calculate are; store in a
+   printf("\nArea of square is %d\n", a);
+}
