361ca259551dfc35598e7a5cbe575ee97826a0d5
FabioMen10 <fabiomendesilva@gmail.com>
2022-11-30 23:28:43 -0400

    Start a new path of C

---
+++ PacktLearnC/Chapter1_HelloWorld/hello2.c
@@ -0,0 +1,15 @@
+/*
+ * hello2.c
+ * My first C program with comments.
+ * by Fábio Mendes
+ * created 2022/11/27
+ */
+ 
+#include <stdio.h>
+int main()
+{
+    printf( "Hello, world!\n" );
+    return 0;
+}
+
+/* eof */
